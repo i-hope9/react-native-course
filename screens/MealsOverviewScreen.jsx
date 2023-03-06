f4e62c73099311cc9b3c55b84a35019397963c5a
Eeejeong Kim <ennea9on@gmail.com>
2023-03-07 08:33:31 +0900

    feat: MealItem 상세보기

--- screens/MealsOverviewScreen.jsx
+++ screens/MealsOverviewScreen.jsx
@@ -1,10 +1,22 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, FlatList, View, Text } from "react-native";
+import MealItem from "../components/MealItem";
 import { MEALS } from "../data/dummy-data";
 export default function MeasOverviewScreen({ route }) {
   const categoryId = route.params.categoryId;
+  const displayedMeals = MEALS.filter((meal) => {
+    return meal.categoryIds.indexOf(categoryId) >= 0;
+  });
+  function renderMealItem(itemData) {
+    return <MealItem title={itemData.item.title}/>
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Meals Overview - {categoryId} </Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      ></FlatList>
     </View>
   );
 }
