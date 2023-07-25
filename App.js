38eb5a55a7c5ac50a16cd80a8439964b3aca1592
i-hope9 <ennes9on@gmail.com>
2023-07-25 20:59:53 +0900

    feat: AllPlaces 헤더 버튼 추가

--- App.js
+++ App.js
@@ -1,19 +1,45 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
 import AllPlaces from "./screens/AllPlaces";
 import AddPlace from "./screens/AddPlace";
 import { StatusBar } from "expo-status-bar";
+import IconButton from "./components/UI/IconButton";
+import { Colors } from "./constants/Colors";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-const Stack = createStackNavigator();
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
       <StatusBar style="dark" />
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="AllPlaces" component={AllPlaces} />
-          <Stack.Screen name="AddPlace" component={AddPlace} />
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: { backgroundColor: Colors.primary500 },
+            headerTintColor: Colors.gray700,
+            contentStyle: { backgroundColor: Colors.gray700 },
+          }}
+        >
+          <Stack.Screen
+            name="AllPlaces"
+            component={AllPlaces}
+            options={({ navigation }) => ({
+              title: "나의 최애 공간",
+              headerRight: ({ tintedColor }) => (
+                <IconButton
+                  icon="add"
+                  color={tintedColor}
+                  size={24}
+                  onPress={() => navigation.navigate("AddPlace")}
+                />
+              ),
+            })}
+          />
+          <Stack.Screen
+            name="AddPlace"
+            component={AddPlace}
+            options={{ title: "최애 공간 추가" }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>
