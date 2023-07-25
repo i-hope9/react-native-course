42463228c8a3719b4d32f6e546f5be18b8a2eb6f
i-hope9 <ennes9on@gmail.com>
2023-07-26 08:25:15 +0900

    feat: 사진 촬영 기능 추가

---
+++ components/Places/ImagePicker.jsx
@@ -0,0 +1,73 @@
+import { Alert, Image, StyleSheet, Text, View } from "react-native";
+import {
+  launchCameraAsync,
+  useCameraPermissions,
+  PermissionStatus,
+} from "expo-image-picker";
+import { useState } from "react";
+import { Colors } from "../../constants/Colors";
+import OutlinedButton from "../UI/OutlinedButton";
+
+export default function ImagePicker() {
+  const [status, requestPermission] = useCameraPermissions();
+  const [pickedImage, setPickedImage] = useState();
+
+  async function verifyPermissions() {
+    if (status.status === PermissionStatus.UNDETERMINED) {
+      const response = await requestPermission();
+      return response.granted;
+    }
+
+    if (status.granted === PermissionStatus.DENIED) {
+      Alert.alert("권한이 없습니다.");
+      return false;
+    }
+
+    return true;
+  }
+
+  async function takeImageHandler() {
+    const permission = await verifyPermissions();
+    if (!permission) {
+      return;
+    }
+
+    const image = await launchCameraAsync({
+      allowsEditing: true,
+      aspect: [16, 9],
+      quality: 0.5,
+    });
+    console.log(image.assets[0]);
+    setPickedImage(image.assets[0].uri);
+  }
+
+  let imagePreview = <Text>추가된 사진이 없습니다.</Text>;
+
+  if (pickedImage) {
+    imagePreview = <Image style={styles.image} source={{ uri: pickedImage }} />;
+  }
+  return (
+    <View>
+      <View style={styles.imagePreview}>{imagePreview}</View>
+      <OutlinedButton icon="camera" onPress={takeImageHandler}>
+        사진 촬영
+      </OutlinedButton>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  imagePreview: {
+    width: "100%",
+    height: 200,
+    marginVertical: 8,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.primary100,
+    borderRadius: 4,
+  },
+  image: {
+    height: "100%",
+    width: "100%",
+  },
+});
